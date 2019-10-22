8a0b95a21b02239139d5663ba0ef4a79e39f11fb
Hasan Shobri <hasan.shobri@outlook.com>
2019-10-22 21:29:40 +0700

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceSandec.Models;
+using ServiceSandec.Repositories;
+
+namespace ServiceSandec.Controllers
+{
+    [Route("api/todo")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoRepository<Todo> _todoRepository;
+
+        public TodoController(ITodoRepository<Todo> todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var todoItems = _todoRepository.GetAll();
+            return Ok(todoItems);
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(long id)
+        {
+            var todoItem = _todoRepository.Get(id);
+
+            if (todoItem == null) return NotFound("Not found");
+
+            return Ok(todoItem);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Todo todoItem)
+        {
+            if (todoItem == null) return BadRequest("data is null.");
+
+            _todoRepository.Add(todoItem);
+            return CreatedAtRoute(
+                "Get",
+                new {todoItem.Id},
+                todoItem);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody] Todo todoItem)
+        {
+            if (todoItem == null) return BadRequest("todo is null.");
+
+            var updateToUpdate = _todoRepository.Get(id);
+            if (updateToUpdate == null) return NotFound("Todo record couldn't be found.");
+
+            _todoRepository.Update(updateToUpdate, todoItem);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todoItem = _todoRepository.Get(id);
+            if (todoItem == null) return NotFound("Todo record couldn't be found.");
+
+            _todoRepository.Delete(todoItem);
+            return NoContent();
+        }
+    }
+}
---
+++ Repositories/TodoRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceSandec.Models;
+
+namespace ServiceSandec.Repositories
+{
+    public class TodoRepository : ITodoRepository<Todo>
+    {
+        private readonly TodoContext _todoContext;
+
+        public TodoRepository(TodoContext context)
+        {
+            _todoContext = context;
+        }
+
+        public IEnumerable<Todo> GetAll()
+        {
+            return _todoContext.TodoItems.OrderByDescending(c => c.Id).ToList();
+        }
+
+        public Todo Get(long id)
+        {
+            return _todoContext.TodoItems
+                .FirstOrDefault(e => e.Id == id);
+        }
+
+        public void Add(Todo entity)
+        {
+            _todoContext.TodoItems.Add(entity);
+            _todoContext.SaveChanges();
+        }
+
+        public void Update(Todo dbEntity, Todo entity)
+        {
+            dbEntity.Name = entity.Name;
+            dbEntity.IsComplete = entity.IsComplete;
+            _todoContext.SaveChanges();
+        }
+
+        public void Delete(Todo entity)
+        {
+            _todoContext.TodoItems.Remove(entity);
+            _todoContext.SaveChanges();
+        }
+    }
+}
